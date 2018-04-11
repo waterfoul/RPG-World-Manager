5eb3965cbe61016302e2e8a7576db65b2eb5eb52
Goose <waterfoul@gmail.com>
2018-04-10 21:00:33 -0500

    Starting the metadata

--- src/repo-manager/index.js
+++ src/repo-manager/index.js
@@ -1,10 +1,14 @@
 import { Repository, Signature } from 'nodegit';
+import { ncp } from 'ncp';
 import path from 'path';
 import fs from 'promise-fs';
+import promisify from 'promisify-node';
+ncp = promisify(ncp);
 
 const sig = Signature.now('RPG-World-Maker', 'invalid@email.ttt');
 
 const dataDir = path.join(__dirname, '..', '..', 'repos');
+const templateDir = path.join(__dirname, '..', 'template');
 fs.stat(dataDir)
     .catch(() => {
         return fs.mkdir(dataDir);
@@ -14,11 +18,15 @@ export function listRepos() {
     return fs.readdir(dataDir);
 }
 
+function getPath(name) {
+    return path.join(path.join(dataDir, name));
+}
+
 export function createRepo(name) {
-    const repoPath = path.join(path.join(dataDir, name));
+    const repoPath = getPath(name);
     return fs.mkdir(repoPath)
         .then(() => Repository.init(repoPath, 0))
-        .then((repo) => /* TODO: copy the template */ fs.writeFile(path.join(repoPath, 'map.json'), '{}').then(() => repo))
+        .then((repo) => ncp(templateDir, repoPath).then(() => repo))
         .then((repo) => repo.createCommitOnHead(
             ['map.json'],
             sig,
@@ -26,4 +34,15 @@ export function createRepo(name) {
             'Initial Commit'
         ).then((commit) => ({repo, commit})))
         .then(({ repo, commit }) => {console.log(commit); repo.createBranch('master', commit, true)});
+}
+
+export function getInfo(name) {
+    const repoPath = getPath(name);
+    return Repository.open(repoPath)
+        .then((repo) => {
+            return repo.getBranch('~~META~~');
+        })
+        .then((metaCommit) => {
+            console.log(metaCommit);
+        })
 }
